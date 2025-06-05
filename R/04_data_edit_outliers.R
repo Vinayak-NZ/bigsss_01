8378b44aa425f1e07eb3da88f48c71d0b35d14af
Vinayak Anand Kumar <v@NAT-INSIDE-192-168-217-100.rrz.uni-hamburg.de.local>
2025-06-05 16:27:07 +0200

    outlier test check and add-in

--- R/04_data_edit_outliers.R
+++ R/04_data_edit_outliers.R
@@ -1,4 +1,22 @@
-## ---- outlier-test
+## ---- outlier-test-SCON
+
+# Compute IQR for SCON
+Q1 <- quantile(data_imputed_pooled_all$SCON, 0.25, na.rm = TRUE)
+Q3 <- quantile(data_imputed_pooled_all$SCON, 0.75, na.rm = TRUE)
+IQR_value <- Q3 - Q1
+
+# Define lower and upper outlier thresholds
+lower_bound <- Q1 - 1.5 * IQR_value
+upper_bound <- Q3 + 1.5 * IQR_value
+
+# Identify outliers
+outliers <- data_imputed_pooled_all %>% 
+  filter(SCON < lower_bound | SCON > upper_bound)
+
+# View potential outliers
+print(outliers)
+
+## ---- outlier-test-WFI
 
 # Compute IQR for WFI
 Q1 <- quantile(data_imputed_pooled_all$WFI, 0.25, na.rm = TRUE)
